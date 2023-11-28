b7a1bef0980b4945516adb79ea2e47b437a8a596
Bharath Vepanjeri <bharathr004@gmail.com>
2023-11-28 12:22:51 +0530

    some changes

--- server/index.js
+++ server/index.js
@@ -1,45 +1,30 @@
-// const { createPool } = require('mysql');
+import express, { json } from "express";
+import { createConnection } from "mysql";
 
-// const db = createPool({
-//   host: 'localhost',
-//   user: 'root',
-//   password: 'connt2db@mysql',
-// });
+import cors from "cors";
 
-// db.query(
-//   `UPDATE store.customers SET first_name="bharath" WHERE customer_id=1`,
-//   (err, res) => {
-//     return console.log(res);
-//   }
-// );
-
-const express = require('express');
-const { createConnection } = require('mysql');
-
-const cors = require('cors');
-
-const db = createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'connt2db@mysql',
-  database: 'locators_emea',
+let db = createConnection({
+  host: "localhost",
+  user: "root",
+  password: "connt2db@mysql",
+  database: "locators_emea",
 });
 
-const app = express();
-app.use(express.json());
+let app = express();
+app.use(json());
 app.use(cors());
 
 app.listen(8080, () => {
-  console.log('Successfully connected to backed server');
+  console.log("Successfully connected to backed server");
 });
 
 /**root*/
 
-app.get('/', (req, res) => {
-  res.json('hello bro this is from backend');
+app.get("/", (req, res) => {
+  res.json("hello bro this is from backend");
 });
 
-app.post('/addLocator', (req, res) => {
+app.post("/addLocator", (req, res) => {
   const { locatorName } = req.body;
   /**INSERT INTO my_table (id, name) VALUES (10, 'John'); */
   const q = `INSERT INTO new_id (Locator_name, MAC_FR, EL_FR, BB_FR) VALUES ("${locatorName}","","","")`;
@@ -59,7 +44,7 @@ app.get(`/showLocator`, (req, res) => {
   });
 });
 
-app.post('/update', (req, res) => {
+app.post("/update", (req, res) => {
   /*UPDATE new_id SET MAC_FR = "*****" WHERE Locator_name = "fname" */
   const { brand, locale, locator, updatedLocator } = req.body;
   const brand_locale = `${brand}_${locale}`;
